88c8426ca7b7cf0b82bb4ef45945e1dbbdab1115
GZ <Innocent1150@gmail.com>
2021-08-19 12:38:10 +0700

    Structure Created

---
+++ ProcessorService.cs
@@ -0,0 +1,17 @@
+using System;
+
+public static class ProcessorService {
+
+    public static int processInput(String input) {
+        return 0;
+    }
+
+    public static String deleteNonNumeric(String input) {
+        return "";
+    }
+
+    public static int convertToNumeric(String numericInput) {
+        return 0;
+    }
+
+}
---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class TestString2Integer {
+    
+    public static void main(String[] args) {
+
+    }
+
+    
+}
